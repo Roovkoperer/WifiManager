7f89470202715810cd3120778f4e447431833e4c
Roovkoperer <38276745+Roovkoperer@users.noreply.github.com>
2018-04-24 22:00:05 +0200

    Initial commit

---
+++ WifiManager.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+ 
+LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x3F for a 16 chars and 2 line display
+ 
+void setup()
+{
+lcd.init(); // initialize the lcd
+lcd.init();
+// Print a message to the LCD.
+lcd.backlight();
+lcd.setCursor(0,0);
+lcd.print("Hallo Maus!");
+lcd.setCursor(0,1);
+lcd.print("Lieb Dich!");
+
+WiFiManager wifiManager;
+
+//first parameter is name of access point, second is the password
+wifiManager.autoConnect("AP-NAME");
+}
+ 
+void loop()
+{
+  delay(2500);
+  lcd.clear();
+  delay(1000);
+  lcd.setCursor(0,0);
+  lcd.print("WLAN!");
+  lcd.setCursor(0,1);
+  lcd.print(String(millis()/1000) + "sec");
+}
